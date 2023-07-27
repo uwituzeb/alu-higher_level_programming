b4c56672dcb25dc21c4b12ca4630c91c582fc1cf
berniceu <berniceuwituze@gmail.com>
2023-07-27 23:29:48 +0200

    aceess and update private attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""
-This is a module that writes a class
-Square that defines a square by: (based on 0-square.py)
-"""
+""" defines a square based on size  """
+
+
 class Square:
-    """Function that defines a square"""
+    """ square with private instance attribute size """
+
     def __init__(self, size):
+
+        """
+        Args:
+            size: size of square
+        """
+
         self.__size = size
-        """
-        Private instance attribute: size
-        Instatation with size
-        (no type/value verification)
-        """
