71c81edbde9a0e63d610f7c06d19bf8bbeea322a
uwituzeb <b.uwituze@alustudent.com>
2023-09-25 21:40:34 +0300

    arguments

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // print 3 lines
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
