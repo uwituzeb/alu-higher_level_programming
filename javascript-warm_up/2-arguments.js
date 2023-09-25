c841217e7c83b50cdf7354a65d4ae46b6fbdd762
uwituzeb <b.uwituze@alustudent.com>
2023-09-25 21:27:44 +0300

    arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// arguments
+
+const args = process.argv.slice(2);
+
+if(args.length === 0){
+	console.log("No argument");
+
+}
+else if (args.length === 1){
+	console.log("Argument found");
+}
+else {
+	console.log("Arguments found");
+}
