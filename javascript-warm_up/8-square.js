70a54352f73d9d17365ebcb6cfb82a4b31c5be4a
bernice uwituze <133651805+uwituzeb@users.noreply.github.com>
2023-09-26 20:36:05 +0300

    Update 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,7 +5,8 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+  for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
+}
 
