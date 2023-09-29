f81d8be5c9b4cedbc011ee56f0ff1163554d7955
bernice uwituze <133651805+uwituzeb@users.noreply.github.com>
2023-09-29 14:13:48 +0300

    fix errors

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-class Rectangle{
-  constructor(w, h) {
-    if (w > 0 && h > 0) { 
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.width; i++) {
       for (let j = 0; j < this.height; j++) {
         process.stdout.write('X');
