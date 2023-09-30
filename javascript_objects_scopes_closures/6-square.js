2d956f44230f77aeab96f2e7d3ffd9b4e40250b2
uwituzeb <b.uwituze@alustudent.com>
2023-09-30 21:39:21 +0300

    extension

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const square = require('./5-square.js');
+
+class Square extends square {
   constructor (size) {
     super(size, size);
   }
